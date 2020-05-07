fd2edf3f812e1c981f5869e8c412dd0546964949
Fasya Khuzaimah <38481704+fasyakhuza@users.noreply.github.com>
2020-05-07 16:51:45 +0800

    Update Top_yieldratio.py

--- Top_yieldratio.py
+++ Top_yieldratio.py
@@ -154,8 +154,8 @@ for i in E1:
     i.Draw()
 
 c1.cd()
-c1.Modified()
-c1.Update()
+#c1.Modified()
+#c1.Update()
 c1.SaveAs("Tope_test.pdf")
 c1.SaveAs("Tope_test.png")
 
@@ -190,8 +190,8 @@ for i in E2:
     i.Draw()
 
 c2.cd()
-c2.Modified()
-c2.Update()
+#c2.Modified()
+#c2.Update()
 c2.SaveAs("Topmu_test.pdf")
 c2.SaveAs("Topmu_test.png")
 
